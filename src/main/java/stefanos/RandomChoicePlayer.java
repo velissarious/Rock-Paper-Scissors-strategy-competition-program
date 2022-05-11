7abba2bedd5405a80b6465e7a966955cd0b614eb
DimitriosStefanosVelissariou <velissarious@gmail.com>
2022-05-11 18:01:45 +0200

    add comment for random choice player

--- src/main/java/stefanos/RandomChoicePlayer.java
+++ src/main/java/stefanos/RandomChoicePlayer.java
@@ -5,6 +5,7 @@ import java.util.SplittableRandom;
 public class RandomChoicePlayer implements Player {
 
 	public Weapon choose() {
+		// This player chooses randomly:
 		SplittableRandom splittableRandom = new SplittableRandom();
 		int randomChoice = splittableRandom.nextInt(0, 3);
 		return Weapon.values()[randomChoice];

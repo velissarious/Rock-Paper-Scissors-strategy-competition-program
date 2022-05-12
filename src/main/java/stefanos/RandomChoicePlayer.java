0d515dcb99065004501d703f62dcb408677d5e66
DimitriosStefanosVelissariou <velissarious@gmail.com>
2022-05-12 12:56:14 +0200

    minor performance fix
    weapon should be converted to array only once for each random choice player

--- src/main/java/stefanos/RandomChoicePlayer.java
+++ src/main/java/stefanos/RandomChoicePlayer.java
@@ -4,11 +4,19 @@ import java.util.SplittableRandom;
 
 public class RandomChoicePlayer implements Player {
 
+	// Store the weapon enumeration as array once to avoid doing in multiple times
+	// for performance reasons.
+	private Weapon[] weaponArray;
+
+	public RandomChoicePlayer() {
+		this.weaponArray = Weapon.values();
+	}
+
 	public Weapon choose() {
 		// This player chooses randomly:
 		SplittableRandom splittableRandom = new SplittableRandom();
 		int randomChoice = splittableRandom.nextInt(0, 3);
-		return Weapon.values()[randomChoice];
+		return this.weaponArray[randomChoice];
 	}
 
 }
